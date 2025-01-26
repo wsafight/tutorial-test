1d111370f02c06d0cdff387d388f9a3aa9bc6b62
jump-and-jump <984292420@qq.com>
2025-01-26 19:24:17 +0800

    feat: add site and base

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,8 @@ import tutorialkit from '@tutorialkit/astro';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: "https://wsafight.github.io",
+  base: "tutorial-test",
   devToolbar: {
     enabled: false,
   },
