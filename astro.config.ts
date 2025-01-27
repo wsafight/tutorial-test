c9a4f5c66db34fa9acb254036cfa3014a72d7006
jump-and-jump <984292420@qq.com>
2025-01-27 16:53:38 +0800

    chore: use vercel

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,6 @@ import tutorialkit from '@tutorialkit/astro';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://wsafight.github.io",
-  base: "tutorial-test",
   devToolbar: {
     enabled: false,
   },
